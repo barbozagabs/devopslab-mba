5b7d0fce6fe2b46a0334fd0605c30073499625a6
Gabriela Barboza <barbozagabis@gmail.com>
2024-03-21 02:02:57 +0000

    Lab Concluido

--- test.py
+++ test.py
@@ -16,4 +16,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Gabs v3.0")
+        self.assertEqual(self.result.data.decode('utf-8'), "Gabs v3.0 - Concluido")
